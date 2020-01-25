27848f9011ca1f971e933464e23b36ce17eaedab
Ilya Brodnikov <brodnikov1950@gmail.com>
2020-01-25 21:56:40 +0400

    Крестики-нолики на JavaFX [#49777]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -1,6 +1,11 @@
 package job4j.tictactoe;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Logic3T {
     private final Figure3T[][] table;
@@ -9,35 +14,50 @@ public class Logic3T {
         this.table = table;
     }
 
-    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
-        boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startX][startY];
-            startX += deltaX;
-            startY += deltaY;
-            if (!predicate.test(cell)) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0,0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1 , 0, -1, 1);
+        return checkWin(x -> x.hasMarkX());
     }
 
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0,0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+        return checkWin(o -> o.hasMarkO());
+    }
+
+    private boolean checkWin(Predicate<Figure3T> who) {
+        boolean rsl = false;
+        int size = table.length;
+        int cl = 0;
+        int cr = 0;
+        for (int i = 0; i < size; i++) {
+            int v = 0;
+            int g = 0;
+            for (int n = 0; n < size; n++) {
+                if (who.test(table[i][n])) {
+                    g++;
+                }
+                if (who.test(table[n][i])) {
+                    v++;
+                }
+            }
+            if (g == size || v == size) {
+                rsl = true;
+                System.out.println(rsl);
+            }
+            if (who.test(table[i][i])) {
+                cl++;
+            }
+            if (who.test(table[i][size - i - 1])) {
+                cr++;
+            }
+        }
+        if (cl == size || cr == size) {
+            rsl = true;
+        }
+        return rsl;
     }
 
     public boolean hasGap() {
-        return true;
+        return Arrays.stream(table).flatMap(Arrays::stream).
+                filter(figure3T -> !figure3T.hasMarkX() && !figure3T.hasMarkO()).
+                collect(Collectors.toList()).size() > 0;
     }
 }
